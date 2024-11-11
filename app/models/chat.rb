5f6379c1ea93f69f027c40a9e22683743c8a2461
nirbelelti <nirbelelti@gmail.com>
2024-11-11 12:02:31 +0100

    Chat model

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,3 +1,24 @@
 class Chat < ApplicationRecord
+  after_create_commit { broadcast_prepend_to 'chats' }
+  after_update_commit { broadcast_replace_to 'chats' }
+  after_destroy_commit { broadcast_remove_to 'chats' }
+
   belongs_to :user
+  validates :history, presence: true, on: :update
+
+  before_update :request_answer
+
+  attr_accessor :question
+
+  private
+
+  def request_answer
+
+    return if self.question.blank?
+
+    response = Gemini::Client.send_message(self.question)
+    reply = response.dig('candidates', 0, 'content', 'parts', 0, 'text') || response['error'] || "Sorry, I don't understand"
+    self.history['massages'] << { "role": 'bot', "content": reply }
+  end
+
 end
