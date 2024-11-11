355b5dcbe5ec2a4cd68d5def45d4563f9efd67f2
nirbelelti <nirbelelti@gmail.com>
2024-11-11 20:16:37 +0100

    Utilise sidekiq job to update db

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -19,6 +19,13 @@ class Chat < ApplicationRecord
     response = Gemini::Client.send_message(self.question)
     reply = response.dig('candidates', 0, 'content', 'parts', 0, 'text') || response['error'] || "Sorry, I don't understand"
     self.history['massages'] << { "role": 'bot', "content": reply }
+    enqueue_add_question_job
+  end
+
+  def enqueue_add_question_job
+    return if self.question.blank?
+
+    AddQuestionJob.perform_async(id, history.deep_stringify_keys)
   end
 
 end
