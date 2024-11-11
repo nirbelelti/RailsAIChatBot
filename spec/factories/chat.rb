b286b12e9c08a788c0b0388f119bbcea75f8bfc3
nirbelelti <nirbelelti@gmail.com>
2024-11-11 12:00:55 +0100

    Factories. Update chat factory

--- spec/factories/chat.rb
+++ spec/factories/chat.rb
@@ -2,5 +2,8 @@ FactoryBot.define do
   factory :chat do
     association :user
 
+    trait :with_history do
+      history { { "massages": [{ "role": 'bot', "content": 'Hello' }] } }
+    end
   end
 end
