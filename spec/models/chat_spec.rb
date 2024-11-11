339adae006e0e40eec286534aa3ab062aa69c27e
nirbelelti <nirbelelti@gmail.com>
2024-11-11 12:01:39 +0100

    Chat model tests

--- spec/models/chat_spec.rb
+++ spec/models/chat_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Chat, type: :model do
-  describe "relation" do
+  describe 'Associations' do
     it { should belong_to(:user) }
-
+  end
+  describe 'Validations' do
+    it { should validate_presence_of(:history).on(:update) }
   end
 
 end
